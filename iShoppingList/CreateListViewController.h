8bbb37b98a75d3705e367668435f0a1a14d292ad
Pierre Flauder <pflauder@gmail.com>
2015-03-10 13:38:38 -0700

    Debut des creations des listes (formulaire sans appels a l'API)

---
+++ iShoppingList/CreateListViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateListViewController.h
+//  iShoppingList
+//
+//  Created by Pierre Flauder on 10/03/2015.
+//  Copyright (c) 2015 Nicolas Save. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateListViewController : UIViewController
+
+@end
---
+++ iShoppingList/CreateListViewController.m
@@ -0,0 +1,37 @@
+//
+//  CreateListViewController.m
+//  iShoppingList
+//
+//  Created by Pierre Flauder on 10/03/2015.
+//  Copyright (c) 2015 Nicolas Save. All rights reserved.
+//
+
+#import "CreateListViewController.h"
+
+@interface CreateListViewController ()
+
+@end
+
+@implementation CreateListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
